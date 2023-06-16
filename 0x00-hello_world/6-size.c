c0e63fcf7a506e195987287c9bbda2431dbd04a6
aaguennach <adnanegu5@gmail.com>
2023-06-15 17:36:06 -0700

    ccc

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+  *main - Entry point
+  *Return: always 0 (success)
+  */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)", sizeof(char));
+	printf("Size of a int: %d byte(s)", sizeof(char))
