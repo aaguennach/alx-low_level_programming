3934916004a47f97c2fe7bc88422ecb0e0c371e2
aaguennach <adnanegu5@gmail.com>
2023-06-18 08:41:04 -0700

    jfjf

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 /**
- * main - possible different combinations of two digits
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -26,4 +25,3 @@ int main(void)
     putchar('\n');
 
     return (0);
-}
