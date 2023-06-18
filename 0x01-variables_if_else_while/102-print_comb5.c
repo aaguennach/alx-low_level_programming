cb735d231ea9b6ccc02440834c452f29db27a278
aaguennach <adnanegu5@gmail.com>
2023-06-18 08:25:14 -0700

    jfjf

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 
 /**
- * main - Entry
- * Prints the alphabet.
- * Return: Always 0 (Success)
+ * main - Entry point
+ *
+ * Description: Prints all possible combinations of two two-digit numbers
+ *              separated by a space and comma.
+ *              Numbers range from 0 to 99.
+ *              Combinations are printed in ascending order.
+ *
+ * Return: 0 (Success)
  */
+int main(void)
+{
+	int i, j;
 
-int main(void) {
-    int i, j;
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			int digit1 = i / 10;
+			int digit2 = i % 10;
+			int digit3 = j / 10;
+			int digit4 = j % 10;
 
-    for (i = 0; i <= 99; i++) {
-        for (j = i + 1; j <= 99; j++) {
-            int digit1 = i / 10;
-            int digit2 = i % 10;
-            int digit3 = j / 10;
-            int digit4 = j % 10;
+			putchar(digit1 + '0');
+			putchar(digit2 + '0');
+			putchar(' ');
+			putchar(digit3 + '0');
+			putchar(digit4 + '0');
 
-            putchar(digit1 + '0');
-            putchar(digit2 + '0');
-            putchar(' ');
-            putchar(digit3 + '0');
-            putchar(digit4 + '0');
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 
-            if (i != 98 || j != 99) {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
+	putchar('\n');
 
-    putchar('\n');
-
-    return 0;
+	return 0;
 }
